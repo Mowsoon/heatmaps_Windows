7633934f3b4cbf490cb3d97729d4c65f50af7b76
Mowsoon <alban.munari@gmail.com>
2025-05-16 21:25:34 +0200

    adding the language in the context of home

--- routers/home.py
+++ routers/home.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Request
 from config import templates
-
+from helpers.html_handler import find_language
 router = APIRouter(
     tags = ["Home"],
     responses={404: {"description": "Not found"}}
@@ -8,9 +8,12 @@ router = APIRouter(
 
 @router.get("/")
 async def root(request: Request):
+    languages, translation = find_language("home", request)
     return templates.TemplateResponse(
         name    = "home.html",
         context = {
-            "request": request
+            "request": request,
+            "current_language": languages,
+            "translations": translation
         }
     )
