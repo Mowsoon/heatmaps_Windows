dcfdeacbaf1754ea677b189b137b3bd220bd8312
Mowsoon <alban.munari@gmail.com>
2025-05-16 22:23:34 +0200

    refactor(config): update template initialization and add new constants for file paths and allowed extensions
    Signed-off-by: Mowsoon <alban.munari@gmail.com>

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
 from fastapi.templating import Jinja2Templates
+from pathlib import Path
+
+template = Jinja2Templates(directory="templates")
 
 
-templates = Jinja2Templates(directory="templates")
+MAPS_DIR = Path("static/maps")
+PREVIEWS_DIR = MAPS_DIR / "previews"
+MAPS_ALLOWED_EXTENSIONS = [".png", ".jpg", ".jpeg", ".pdf"]
+
+DATA_DIR = Path("static/data")
+
