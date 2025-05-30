afd7e481220f6f8ab278f6f0061d9ab759853b19
Mowsoon <alban.munari@gmail.com>
2025-05-30 23:14:41 +0200

    feat(helpers): add `update_json_with_scan` to save WiFi scan results to JSON
    Signed-off-by: Mowsoon <alban.munari@gmail.com>

--- helpers/data_handler.py
+++ helpers/data_handler.py
@@ -2,6 +2,8 @@ from fastapi import File, UploadFile, status, HTTPException
 from fastapi.responses import RedirectResponse, FileResponse
 from pathlib import Path
 from config import DATA_DIR
+import json
+from helpers.scan import extract_ssid
 
 
 
@@ -29,4 +31,34 @@ def send_data(map_name: str):
         media_type='application/json',
         headers={"Content-Disposition": f"attachment; filename={map_name}.json"}
     )
-    
+
+def update_json_with_scan(map_name: str):
+    file_path = DATA_DIR / f"{map_name}.json"
+
+    if file_path.exists():
+        with open(file_path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+    else:
+        data = {}
+
+    results = extract_ssid()
+
+    for network in results:
+        ssid    = network["ssid"]
+        band    = network["band"]
+        bssid   = network["bssid"]
+        signal  = network["signal"]
+
+        key = f"{ssid} [{band}]"
+        if key not in data:
+            data[key] = []
+
+        data[key].append({
+            "bssid": bssid,
+            "signal": signal
+        })
+
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+
+    return {"status": "success", "message": f"Scan data saved to {file_path.name}"}
