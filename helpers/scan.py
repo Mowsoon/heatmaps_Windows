eb0d83cb7527021c75163e65e79f063ef11bda73
Mowsoon <alban.munari@gmail.com>
2025-05-30 22:32:25 +0200

    feat(helpers): add function to determine WiFi band based on frequency
    Signed-off-by: Mowsoon <alban.munari@gmail.com>

--- helpers/scan.py
+++ helpers/scan.py
@@ -8,3 +8,13 @@ def wifi_scan():
     iface.scan()
     time.sleep(3)
     return iface.scan_results()
+
+def get_band(freq):
+    if 2400 <= freq <= 24835:
+        return "2.4GHz"
+    elif 5150 <= freq <= 5825:
+        return "5GHz"
+    elif 5925 <= freq <= 7125:
+        return "6GHz"
+    else:
+        return "Unknown"
