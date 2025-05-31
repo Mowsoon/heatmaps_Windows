77b8b4981bd36138ab527eca2585ab7f2f2c969a
Mowsoon <alban.munari@gmail.com>
2025-05-31 13:10:34 +0200

    feat(scans): add endpoints for loading and updating scan maps
    Signed-off-by: Mowsoon <alban.munari@gmail.com>

--- routers/scans.py
+++ routers/scans.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Request, File, UploadFile
-from config import template
+from fastapi import APIRouter, Request, File, UploadFile, HTTPException
+from fastapi.responses import HTMLResponse, JSONResponse
+from config import template, ClickPosition
 from helpers.html_handler import generate_preview, find_language
-from helpers.file_handler import load_file
+from helpers.file_handler import load_file, find_map_url
+from helpers.data_handler import delete_json, update_json_with_scan
 
 
 router = APIRouter(
@@ -26,3 +28,26 @@ async def scans(request: Request):
 @router.post("/")
 async def upload_map(file: UploadFile = File(...)):
     return load_file("/scans", file)
+
+
+@router.get("/{map_name}", response_class=HTMLResponse)
+async def load_scan(map_name: str, request: Request):
+    delete_json(map_name)
+    lang, translations = find_language("scan_map", request)
+    map_url = find_map_url(map_name)
+    if map_url:
+        return template.TemplateResponse("scan_map.html", {
+            "request": request,
+            "map_name": map_name,
+            "map_url": map_url,
+            "translations": translations,
+            "current_lang": lang
+        })
+    raise HTTPException(status_code=404, detail="Map not found")
+
+
+@router.post("/{map_name}")
+async def update_scan(map_name: str, position: ClickPosition):
+    update_json_with_scan(map_name, position.x, position.y)
+    return JSONResponse(
+        content={"status": "success", "message": f"Added scan for {map_name} at ({position.x}, {position.y})"})
