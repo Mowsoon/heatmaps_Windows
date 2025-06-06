0ca918fefe20cee0b84faf51b1fe93a23f3309e1
analyse_wifi <alban.munari@gmail.com>
2025-06-06 14:05:20 +0200

    fixing the bug where the page wasn't return correctly if there is a special caracter in the map name

--- routers/change_language.py
+++ routers/change_language.py
@@ -1,14 +1,22 @@
 from fastapi import APIRouter, status, Request
 from fastapi.responses import RedirectResponse
+from urllib.parse import urlparse
 
 router = APIRouter(prefix="/change_language")
+
+from urllib.parse import urlparse
 
 @router.get("/{lang}")
 async def change_language(lang: str, request: Request):
     if lang not in ['fr', 'en']:
         lang = 'en'
     next_url = request.query_params.get("next", "/")
-    redirect_response = RedirectResponse(url=next_url, status_code = status.HTTP_303_SEE_OTHER)
+    
+    if urlparse(next_url).netloc:
+        next_url = "/"
+    
+    redirect_response = RedirectResponse(url=next_url, status_code=status.HTTP_303_SEE_OTHER)
     redirect_response.set_cookie(key="lang", value=lang)
     return redirect_response
+
 
