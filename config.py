91a64f33e94c6c6ada0e4efecf62abbd5c3391d5
Mowsoon <alban.munari@gmail.com>
2025-06-07 17:14:37 +0200

    refactor(config): use dynamic base directory to handle frozen executable paths
    Signed-off-by: Mowsoon <alban.munari@gmail.com>

--- config.py
+++ config.py
@@ -1,15 +1,20 @@
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 from pydantic import BaseModel
+import sys
 
 template = Jinja2Templates(directory="templates")
 
+if getattr(sys, 'frozen', False):
+    BASE_DIR = Path(sys.executable).parent
+else:
+    BASE_DIR = Path(__file__).parent
 
-MAPS_DIR = Path("static/maps")
+MAPS_DIR = BASE_DIR / "static/maps"
 MAPS_ALLOWED_EXTENSIONS = [".png", ".jpg", ".jpeg", ".pdf"]
 MAPS_POSSIBLE_EXTENSIONS = [".png", ".jpg", ".jpeg"]
 
-DATA_DIR = Path("static/data")
+DATA_DIR = BASE_DIR / "static/data"
 
 class ClickPosition(BaseModel):
     x: int
