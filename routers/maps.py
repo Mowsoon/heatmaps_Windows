925774f616b7d6aff819e9d47847cbe2f347a428
Mowsoon <alban.munari@gmail.com>
2025-06-08 11:18:34 +0200

    feat(maps): add `display` endpoint to fetch and display data by map name and key
    Signed-off-by: Mowsoon <alban.munari@gmail.com>

--- routers/maps.py
+++ routers/maps.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Request, status, HTTPException
 from config import template
 from helpers.html_handler import find_language, list_data
 from helpers.file_handler import find_map_url
-from helpers.data_handler import find_ssid_list
+from helpers.data_handler import find_ssid_list, find_data_list
+from urllib.parse import unquote
 
 router = APIRouter(
     prefix="/maps",
@@ -40,3 +41,12 @@ async def heatmaps(map_name: str, request: Request):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data not found")
         
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Map not found")
+
+@router.get("/{map_name}/data/{ssid_band_key:path}")
+async def display(map_name: str, ssid_band_key: str):
+    map_name = unquote(map_name)
+    ssid_band_key = unquote(ssid_band_key)
+
+    data = find_data_list(map_name, ssid_band_key)
+    if data:
+        print(data)
