d151e383ad174081d0ae3f9a326e7a89e076fc33
analyse_wifi <alban.munari@gmail.com>
2025-06-19 15:45:48 +0200

    blocking the access to the docs and adding the router for the documentation page

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from routers import home, plans, scans, maps, change_language, data
+from routers import home, plans, scans, maps, help, change_language, data
 from config import BASE_DIR
-app = FastAPI()
+
+app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
 app.mount("/static", StaticFiles(directory=BASE_DIR/"static"), name="static")
 app.include_router(home.router)
 app.include_router(plans.router)
 app.include_router(scans.router)
 app.include_router(maps.router)
+app.include_router(help.router)
 app.include_router(change_language.router)
 app.include_router(data.router)
 
