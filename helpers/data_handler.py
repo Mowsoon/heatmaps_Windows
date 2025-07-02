338b4ea30ef160dcac9e8026b410c5814be32508
analyse_wifi <alban.munari@gmail.com>
2025-07-02 09:52:36 +0200

    managing the scan in a new repository

--- helpers/data_handler.py
+++ helpers/data_handler.py
@@ -1,9 +1,9 @@
 from fastapi import File, UploadFile, status, HTTPException
 from fastapi.responses import RedirectResponse, FileResponse
 from pathlib import Path
-from config import SIGNAL_DIR
+from config import SIGNAL_DIR, CHANNEL_DIR
 import json
-from helpers.scan_handler import extract_ssid
+from helpers.scan_handler import extract_scan
 
 
 
@@ -32,7 +32,7 @@ def send_data(map_name: str):
         headers={"Content-Disposition": f"attachment; filename={map_name}.json"}
     )
 
-def update_json_with_scan(map_name: str, x: int, y: int):
+def extract_signal(results, x, y, map_name):
     file_path = SIGNAL_DIR / f"{map_name}.json"
 
     if file_path.exists():
@@ -41,37 +41,73 @@ def update_json_with_scan(map_name: str, x: int, y: int):
     else:
         data = {}
 
-    results = extract_ssid()
-
     for network in results:
         ssid    = network["ssid"]
         band    = network["band"]
         bssid   = network["bssid"]
         signal  = network["signal"]
-
+    
         key = f"{ssid} [{band}]"
         if key not in data:
             data[key] = []
-
+    
         data[key].append({
             "bssid": bssid,
             "signal": signal,
             "x": x,
             "y": y
         })
+    
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+    return f"Scan data saved to {file_path.name}"
+
+def extract_channel(channels: dict[int, int], x: int, y: int, map_name: str):
+    file_path = CHANNEL_DIR / f"{map_name}.json"
+
+    if file_path.exists():
+        with open(file_path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+    else:
+        data = {}
+
+    key = f"{x},{y}"
+
+    data[key] = {f"channel{chan}": count for chan, count in channels.items()}
 
     with open(file_path, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
+    return f"Scan data saved to {file_path.name}"
 
-    return {"status": "success", "message": f"Scan data saved to {file_path.name}"}
+def update_json_with_scan(map_name: str, x: int, y: int):
+    results, channels   = extract_scan()
+    signal_mess         = extract_signal(results, x, y, map_name)
+    channel_mess        = extract_channel(channels, x, y, map_name)
 
-def delete_json(map_name: str):
+    return {"status": "success", "message": signal_mess + "\n" + channel_mess}
+
+
+
+def delete_signal(map_name: str):
     file_path = SIGNAL_DIR / f"{map_name}.json"
     if file_path.exists():
         file_path.unlink()
-        return {"status": "success", "message": f"{file_path.name} has been deleted"}
+        return f"{file_path.name} has been deleted"
     else:
-        return {"status": "error", "message": f"{file_path.name} does not exist"}
+        return f"{file_path.name} does not exist"
+
+def delete_channel(map_name: str):
+    file_path = CHANNEL_DIR / f"{map_name}.json"
+    if file_path.exists():
+        file_path.unlink()
+        return f"{file_path.name} has been deleted"
+    else:
+        return f"{file_path.name} does not exist"
+
+def delete_json(map_name: str):
+    signal_mess     = delete_signal(map_name)
+    channel_mess    = delete_channel(map_name)
+    return {"status": "deleted", "message": signal_mess + "\n" + channel_mess}
 
 def find_ssid_list(map_name: str):
     json_path = SIGNAL_DIR / f"{map_name}.json"
