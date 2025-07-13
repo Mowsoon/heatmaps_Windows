4d8abe3225379c2d78e5e46299927e9778bb0c17
Mowsoon <alban.munari@gmail.com>
2025-07-13 23:47:21 +0200

    refactor(server): update `uvicorn.run` to use explicit ASGI app path and localhost
    Signed-off-by: Mowsoon <alban.munari@gmail.com>

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ app.include_router(data.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
